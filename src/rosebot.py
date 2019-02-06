3df48145c89aa8efeac5ad596fb287a9955a355d
pollactd <pollactd@rose-hulman.edu>
2019-02-06 14:39:36 -0500

    Changed Lower_arm

--- src/rosebot.py
+++ src/rosebot.py
@@ -234,6 +234,11 @@ class ArmAndClaw(object):
         """
 
     def lower_arm(self):
+        self.motor.turn_on(-100)
+        while True:
+            if self.motor.get_position()==0:
+                self.motor.turn_off()
+                break
         """
         Lowers the Arm until it is all the way down, i.e., position 0.
         The robot must have previously calibrated its Arm.
