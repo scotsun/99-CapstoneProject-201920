f9f3e9776c98e8a217476683b608e4be5dfac47b
pollactd <pollactd@rose-hulman.edu>
2019-02-06 13:59:51 -0500

    my name in the file

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -1,7 +1,7 @@
 """
   Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
   Author:  Your professors (for the framework)
-    and Daniel Pollack.
+    and Daniel Pollack..
   Winter term, 2018-2019.
 """
 
