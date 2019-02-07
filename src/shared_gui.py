c92cfcaa6b5153cd708cd3c6bf3b2dc4998cd7cb
wilcoxeb <wilcoxeb@rose-hulman.edu>
2019-02-07 14:26:06 -0500

    wrote the methods for handlers for buttons in the control frame

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -250,6 +250,8 @@ def handle_quit(mqtt_sender):
     Tell the robot's program to stop its loop (and hence quit).
       :type  mqtt_sender:  com.MqttClient
     """
+    print('Check')
+    mqtt_sender.send_message('quit')
 
 
 def handle_exit(mqtt_sender):
@@ -258,3 +260,6 @@ def handle_exit(mqtt_sender):
     Then exit this program.
       :type mqtt_sender: com.MqttClient
     """
+    print('Blub')
+    handle_quit(mqtt_sender)
+    exit()
