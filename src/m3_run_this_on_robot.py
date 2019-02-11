e5767108ce3f56bd403e2a9b89774b14933a3006
wilcoxeb <wilcoxeb@rose-hulman.edu>
2019-02-11 14:04:17 -0500

    updated the rosebot file all of it

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -46,12 +46,15 @@ def run_test_lower_arm():
 
 def real_thing():
     robot = rosebot.RoseBot()
-    delegate_that_recieves = shared_gui_delegate_on_robot.DelegateThatRecieves(robot)
+    delegate_that_recieves = shared_gui_delegate_on_robot.DelegateThatReceives(robot)
     mqtt_reciever = com.MqttClient(delegate_that_recieves)
     mqtt_reciever.connect_to_pc()
 
     while True:
         time.sleep(0.01)
+        if delegate_that_recieves.is_time_to_stop:
+            break
+
 
 
 # -----------------------------------------------------------------------------
